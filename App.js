d4e7819f7af56f89a9450fc37c5bcf7ecce45964
adnan1naeem <adnan1naeem@gmail.com>
2020-11-25 23:08:26 +0500

    fix(TodoApp): TodoApp

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {ScrollView, StyleSheet, Text, View } from 'react-native';
 
+import {Button,TextInput} from 'react-native-paper'
+let id=0;
 export default function App() {
+  const [todoItem,setItem]=React.useState('');
+  const [todoList,setTodoList]=React.useState([]);
+  
+const  InputHandler=(value)=>setItem(value);
+
+const  AddItem=()=>{
+  setItem("");
+  console.log('list',todoList);
+  todoList.push({name:todoItem,id:id++});
+  console.log(todoList);
+  setTodoList([...todoList]);
+}
+
+const  DeleteItem=(id)=>()=>{
+  const filter=todoList.filter((item)=>item.id!=id);
+  setTodoList([...filter]);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+      placeholder='Enter Todo Item'
+      style={{width:'100%'}}
+        value={todoItem}
+        onChangeText={InputHandler}
+      />
+    <Button style={styles.margin} mode="contained"  title="Add" onPress={AddItem}>Add</Button>
+    {
+    todoList.length>0
+    ?
+    todoList.map((item,index)=>{
+      return(
+        <View  key={index} style={{flexDirection:'row'}}>
+          <Text>{item.name}</Text>
+          <Button title="X"  onPress={DeleteItem(item.id)}>X</Button>
+        </View>
+      );    
+    })
+    :
+    null
+  }
     </View>
   );
 }
@@ -18,4 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  margin:{
+    marginTop:10
+  },
+  row:{
+    flexDirection:'row',
+  }
 });
